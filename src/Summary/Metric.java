6f80b430ccf3eb8d4be1474d54a7aff7c5174472
s3878707 <s3878707@rmit.edu.vn>
2021-09-01 18:30:36 +0700

    add attribute for Metric.java

--- src/Summary/Metric.java
+++ src/Summary/Metric.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 public class Metric {
     private ArrayList<ArrayList<Integer>> metricList= new ArrayList<>();
     private ArrayList<ArrayList<Integer>> metricListForUpTo= new ArrayList<>();
+    private ArrayList<ArrayList<String>> dateList = new ArrayList<>();
+    private ArrayList<ArrayList<String>> dateListForUpTo = new ArrayList<>();
     public Metric (){}
 
     /**
@@ -28,10 +30,15 @@ public class Metric {
                 //MAKE A LIST OF GROUPS OF NEWCASES IN SELECTED DATA
                 ArrayList<Integer> newCasesList = new ArrayList<>();
                 ArrayList<Integer> newCaseListFromTheBeginning = new ArrayList<>();
+                ArrayList<String> dateList = new ArrayList<>();
+                ArrayList<String> dateListFromTheBeginning = new ArrayList<>();
                 for (Data n : i) {
                     newCasesList.add(n.getNewCases());
+                    dateList.add(n.getLocalDate());
+
                 }
                 metricList.add(newCasesList);
+                this.dateList.add(dateList);
                 //MAKE A LIST OF GROUP OF NEWCASES FROM THE VERY FIRST BEGINNING OF A COUNTRY TO LAST DATA
                 Data lastDataOfGroup = i.get(i.size() - 1);
                 Data newlastDataOfGroup = new Data();
@@ -47,9 +54,11 @@ public class Metric {
                     if (j.getLocation().equals(lastDataOfGroup.getLocation()) &&
                             processedDataList.indexOf(j) <= processedDataList.indexOf(newlastDataOfGroup.data)) {
                         newCaseListFromTheBeginning.add(j.getNewCases());
+                        dateListFromTheBeginning.add(j.getLocalDate());
                     }
                 }
                 metricListForUpTo.add(newCaseListFromTheBeginning);
+                dateListForUpTo.add(dateListFromTheBeginning);
             }
 
         }
@@ -58,10 +67,14 @@ public class Metric {
                 //MAKE A LIST OF GROUPS OF NEWDEATHS IN SELECTED DATA
                 ArrayList<Integer> newDeathsList = new ArrayList<>();
                 ArrayList<Integer> newDeathsListFromTheBeginning = new ArrayList<>();
+                ArrayList<String> dateList = new ArrayList<>();
+                ArrayList<String> dateListFromTheBeginning = new ArrayList<>();
                 for (Data n : i) {
                     newDeathsList.add(n.getNewDeaths());
+                    dateList.add(n.getLocalDate());
                 }
                 metricList.add(newDeathsList);
+                this.dateList.add(dateList);
                 //MAKE A LIST OF GROUP OF NEWDEATHS FROM THE VERY FIRST BEGINNING OF A COUNTRY TO LAST DATA
                 Data lastDataOfGroup = i.get(i.size() - 1);
                 Data newlastDataOfGroup = new Data();
@@ -77,9 +90,11 @@ public class Metric {
                     if (j.getLocation().equals(lastDataOfGroup.getLocation()) &&
                             processedDataList.indexOf(j) <= processedDataList.indexOf(newlastDataOfGroup.data)) {
                         newDeathsListFromTheBeginning.add(j.getNewCases());
+                        dateListFromTheBeginning.add(j.getLocalDate());
                     }
                 }
                 metricListForUpTo.add(newDeathsListFromTheBeginning);
+                dateListForUpTo.add(dateListFromTheBeginning);
             }
         }
         if (option.equals("c")) {
@@ -87,10 +102,14 @@ public class Metric {
                 //MAKE A LIST OF GROUPS OF NEWVACCINATED IN SELECTED DATA
                 ArrayList<Integer> newVaccinated = new ArrayList<>();
                 ArrayList<Integer> newVaccinatedListFromTheBeginning = new ArrayList<>();
+                ArrayList<String> dateList = new ArrayList<>();
+                ArrayList<String> dateListFromTheBeginning = new ArrayList<>();
                 for (Data n : i) {
                     newVaccinated.add(n.getVaccinated());
+                    dateList.add(n.getLocalDate());
                 }
                 metricList.add(newVaccinated);
+                this.dateList.add(dateList);
                 //MAKE A LIST OF GROUP OF NEWVACCINATED FROM THE VERY FIRST BEGINNING OF A COUNTRY TO LAST DATA
                 Data lastDataOfGroup = i.get(i.size() - 1);
                 Data newlastDataOfGroup = new Data();
@@ -106,9 +125,11 @@ public class Metric {
                     if (j.getLocation().equals(lastDataOfGroup.getLocation()) &&
                             processedDataList.indexOf(j) <= processedDataList.indexOf(newlastDataOfGroup.data)) {
                         newVaccinatedListFromTheBeginning.add(j.getNewCases());
+                        dateListFromTheBeginning.add(j.getLocalDate());
                     }
                 }
                 metricListForUpTo.add(newVaccinatedListFromTheBeginning);
+                dateListForUpTo.add(dateListFromTheBeginning);
             }
         }
     }
@@ -118,7 +139,8 @@ public class Metric {
     public ArrayList<ArrayList<Integer>> getMetricListForUpTo(){
         return metricListForUpTo;
     }
-
+    public ArrayList<ArrayList<String>> getDateList(){return dateList;}
+    public ArrayList<ArrayList<String>> getDateListForUpTo(){return dateListForUpTo;}
     public static void main(String[] args) {
 
     }
