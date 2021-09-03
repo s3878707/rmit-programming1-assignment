7f328f0b4539e244d5b18305057c33d9d9541e66
s3878023 <s3878023@rmit.edu.vn>
2021-09-03 17:39:41 +0700

    changing the name in Display.java adding the comments

--- src/Display/Display.java
+++ src/Display/Display.java
@@ -161,8 +161,11 @@ import Summary.Results;
 import java.util.*;
 
 public class Display {
-    
+
+    //create method tabular display//
+    //Display the table with the existing metrics and results//
     public void tabularDisplay(Results results, Metric metric, String resultType){
+        //setting the solution for up_to option//
         ArrayList<String> rangeList = new ArrayList<>();
         if (resultType.equals("up_to")){
             for (ArrayList<String> i : metric.getDateListForUpTo()) {
@@ -185,6 +188,7 @@ public class Display {
             System.out.println("-".repeat(33));
             System.out.println();
 
+            //setting the solution for new_total option//
         }else if (resultType.equals("new_total")){
             for (ArrayList<String> i : metric.getDateList()) {
                 if (i.size() > 1) {
@@ -210,113 +214,118 @@ public class Display {
         }
     }
     public void chartDisplay(Results results){
+        //display the chart with the chosen materials//
         long[] data = new long[results.getResults().size()];
         for (int i = 0 ; i < results.getResults().size();i++){
             data[i] = results.getResults().get(i);
         }
         long[] organizeData = bubbleSort(data, "value", "descending");
         long[] organizeIndex = {3,2,4,5,1,6};
-        long powerCounter = 1;
-        long groupNumber = organizeData.length;
+        long counter = 1;
+        long grpNum = organizeData.length;
         while (true){
-            if (Math.pow(10,powerCounter)>=organizeData[0]){
+            //loop the counter equal the data that has been sorted in previous step//
+            if (Math.pow(10,counter)>=organizeData[0]){
                 break;
             }else {
-                powerCounter++;
+                counter++;
             }
         }
         long groupIndex = 0;
         long spaceCounter = 0;
         long labelLength = 6;
-        long totalSpace = spaceCounter*groupNumber+labelLength*groupNumber;
+        long totalSpace = spaceCounter*grpNum+labelLength*grpNum;
         while (totalSpace<=80){
             spaceCounter++;
-            totalSpace = spaceCounter*groupNumber+labelLength*groupNumber;
+            totalSpace = spaceCounter*grpNum+labelLength*grpNum;
         }
+        //Working on the drawing section of the chart display //
         spaceCounter--;
         for (int i = 0; i < 23; i++) {
             System.out.print("|");
             long tab_count=0;
-            ArrayList<Long> indexInARow = new ArrayList<Long>();
+            //using the loop to run through the chart//
+            ArrayList<Long> rowInd = new ArrayList<Long>();
             for (int j = 0; j < organizeData.length; j++) {
-                int rowScale = (int) Math.ceil((23 * organizeData[j]) / (organizeData[0]+Math.pow(10, powerCounter-1)));
+                int rowScale = (int) Math.ceil((23 * organizeData[j]) / (organizeData[0]+Math.pow(10, counter-1)));
                 if (rowScale==(23-i)){
-                    indexInARow.add(bubbleSort(data, "index", "descending")[j]);
+                    rowInd.add(bubbleSort(data, "index", "descending")[j]);
                 }
             }
-            long[] indexSort = new long[indexInARow.size()];
-            for (int k = 0; k < indexInARow.size(); k++) {
-                indexSort[k] = indexInARow.get(k);
+            //bring up the new array to get the total number of different groups
+            long[] sortInd = new long[rowInd.size()];
+            for (int k = 0; k < rowInd.size(); k++) {
+                sortInd[k] = rowInd.get(k);
             }
-            indexSort = bubbleSort(indexSort, "value", "ascending");
+            sortInd = bubbleSort(sortInd, "value", "ascending");
             for (int k = 0; k < (spaceCounter+labelLength/2-1); k++) {
                 System.out.print(" ");
             }
-            int starInARow = 0;
-            for (int p = 0; p < indexSort.length; p++) {
-                while (tab_count<(indexSort[p]-1)*(spaceCounter+labelLength)-starInARow){
+            //placing the * elements into demonstrating the value
+            int starCounter = 0;
+            for (int p = 0; p < sortInd.length; p++) {
+                while (tab_count<(sortInd[p]-1)*(spaceCounter+labelLength)-starCounter){
                     System.out.print(" ");
                     tab_count++;
                 }
                 System.out.print("*");
-                starInARow++;
+                starCounter++;
             }
             System.out.println();
         }
+        //drawing the line for the both axis//
         System.out.print("|");
         for (int q = 0; q < 79; q++) {
             System.out.print("_");
         }
         System.out.println();
-        for (int i = 1; i <= groupNumber; i+=1) {
+        for (int i = 1; i <= grpNum; i+=1) {
             for (int j = 0; j < spaceCounter; j++) {
                 System.out.print(" ");
             }
             System.out.print("group"+i);
         }
     }
-    static long[] bubbleSort(long[] originalArray, String returnType, String sortOrder) {
-        long n = originalArray.length;
-        long[] arrayIndex = new long[originalArray.length];
-        for (int i = 0; i < arrayIndex.length; i++) {
-            arrayIndex[i] = i+1;
+    static long[] bubbleSort(long[] orgAr, String returnVal, String ordSorter) {
+        long n = orgAr.length;
+        long[] arrInd = new long[orgAr.length];
+        for (int i = 0; i < arrInd.length; i++) {
+            arrInd[i] = i+1;
         }
-        long[] sortedArray = originalArray.clone();
-        long tempValue = 0;
-        long tempIndex = 0;
-        if (sortOrder.equals("descending")){
+        long[] arrSorter = orgAr.clone();
+        long instntVal = 0;
+        long instntInd = 0;
+        if (ordSorter.equals("descending")){
             for(int i=0; i < n; i++){
                 for(int j=1; j < (n-i); j++){
-                    if(sortedArray[j-1] < sortedArray[j]){
-                        //swap elements and index
-                        tempValue = sortedArray[j-1];
-                        tempIndex = arrayIndex[j-1];
-                        sortedArray[j-1] = sortedArray[j];
-                        arrayIndex[j-1] = arrayIndex[j];
-                        sortedArray[j] = tempValue;
-                        arrayIndex[j] = tempIndex;
+                    if(arrSorter[j-1] < arrSorter[j]){
+                        instntVal = arrSorter[j-1];
+                        instntInd = arrInd[j-1];
+                        arrSorter[j-1] = arrSorter[j];
+                        arrInd[j-1] = arrInd[j];
+                        arrSorter[j] = instntVal;
+                        arrInd[j] = instntInd;
                     }
                 }
             }
-        }else if (sortOrder.equals("ascending")){
+        }else if (ordSorter.equals("ascending")){
             for(int i=0; i < n; i++){
                 for(int j=1; j < (n-i); j++){
-                    if(sortedArray[j-1] > sortedArray[j]){
-                        //swap elements and index
-                        tempValue = sortedArray[j-1];
-                        tempIndex = arrayIndex[j-1];
-                        sortedArray[j-1] = sortedArray[j];
-                        arrayIndex[j-1] = arrayIndex[j];
-                        sortedArray[j] = tempValue;
-                        arrayIndex[j] = tempIndex;
+                    if(arrSorter[j-1] > arrSorter[j]){
+                        instntVal = arrSorter[j-1];
+                        instntInd = arrInd[j-1];
+                        arrSorter[j-1] = arrSorter[j];
+                        arrInd[j-1] = arrInd[j];
+                        arrSorter[j] = instntVal;
+                        arrInd[j] = instntInd;
                     }
                 }
             }
         }
-        if (returnType.equals("value")){
-            return sortedArray;
-        }else if (returnType.equals("index")){
-            return arrayIndex;
+        if (returnVal.equals("value")){
+            return arrSorter;
+        }else if (returnVal.equals("index")){
+            return arrInd;
         }else{
             long[] invalidResult = {0,0,0,0,0,0};
             return invalidResult;
